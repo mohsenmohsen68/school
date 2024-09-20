a8afca690f4f8c18db5cc8690d8de69b591a9ba7
mohsen6831 <mohsenmovahedinejad@gmail.com>
2024-09-20 17:32:43 +0330

    post preview handler developed ...

--- src/components/PostPreviewHandler/PostPreviewHandler.tsx
+++ src/components/PostPreviewHandler/PostPreviewHandler.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Parser from 'html-react-parser'
+import './../../app/globals.css'
+import { useDispatch } from 'react-redux'
+import { selectOption } from '@/Redux/CMS/CMSRoutes'
 
-export default function PostPreviewHandler() {
+
+
+export default function PostPreviewHandler(props) {
+  const dispatch = useDispatch()
+  console.log('course body preview : ',props.data)
   return (
-    <div>
-      
-    </div>
-  )
+  <>
+  <div className='w-full border rounded-md font-dana p-4 px-8 ck-content '>
+    {Parser(props.data.postBody)}
+
+  </div> 
+    <button className='rounded-md bg-green-600 hover:bg-green-400 p-2 mt-2 text-xl font-moraba w-full' onClick={()=>{dispatch( selectOption("") ) }}>
+                بازگشت
+    </button>
+  </>
+   )
 }
